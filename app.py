7a0012c4466c6abce65a9351a006be96bd35601d
unknown <laurindodumba>
2018-05-17 20:21:42 -0300

    Pega os links de cada filme

--- app.py
+++ app.py
@@ -1,11 +1,30 @@
 from bs4 import BeautifulSoup
 import requests
 
-r = requests.get('http://www.gartenshopping.com.br/cinema')
-response_text = r.text
+URL='http://www.gartenshopping.com.br'
 
-parsed_html = BeautifulSoup(response_text, 'html.parser')
-cinema_text = parsed_html.body.find('div', attrs={'class':'cinema-date-filter'})
+def pega_links():
+    links = []
+    r = requests.get(URL + '/cinema')
+    response_text = r.text
 
-for link in cinema_text.find_all('a'):
-    print(link.get('href'))
+    parsed_html = BeautifulSoup(response_text, 'html.parser')
+    cinema_text = parsed_html.body.find('div', attrs={'class': 'item-list-content'})
+
+    for link in cinema_text.find_all('a'):
+        links.append(link.get('href'))
+    
+    return links
+      
+
+def informacao_do_filme(link):
+    r = requests.get(URL + link)
+    response_text = r.text
+
+    parsed_html = BeautifulSoup(response_text, 'html.parser')
+    return parsed_html
+
+
+links = pega_links()
+for link in links:
+    print(informacao_do_filme(link))
